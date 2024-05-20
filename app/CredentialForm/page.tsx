87cafe82f00515fa881e6e321be3823d6d36ec9a
AmrNabel <121950000+AmrNabel@users.noreply.github.com>
2024-05-20 15:12:04 +0300

    first try

--- app/CredentialForm/page.tsx
+++ app/CredentialForm/page.tsx
@@ -1,42 +1,20 @@
 "use client";
 import React, { useEffect } from "react";
 import Image from "next/image";
-import { Box, Typography, useMediaQuery,Theme } from "@mui/material";
-import { useTheme } from '@mui/system';
+import { Box, Typography, useMediaQuery, Theme } from "@mui/material";
+import { useTheme } from "@mui/system";
 import Form from "../components/form/Form";
 import fram from ".././Assets/Frame 35278.png";
 import vector from ".././Assets/Vector 145.png";
 import img3 from ".././Assets/Tessa Persona large sceens.png";
 import { SVGLargeScreen } from "../Assets/SVGs";
-// import { GoogleDriveStorage } from "trust-storage";
 
 const FormComponent = () => {
   const theme: Theme = useTheme();
-  const isLargeScreen = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
-  // async function createFolderAndUploadFile() {
-  //   try {
-  //     const storage = new GoogleDriveStorage(
-  //       "ya29.a0AXooCguvQYvScV8ciYoNaP69soAOpNu-G8Z1Fmpzfv30V494F7PWpLTxT7m40ZlnwyFgVRAqlbYD5AcaF3vy9uQxrc_1GKB9jrZf78o6Ip46QmQHL8gm8wxp2liJ5Q-NqFgicK43B6tq3xR8JngSu6DQf9BO847M04kNaCgYKAaESARASFQHGX2MivjxJ8TrL8MAGqNTsB1UjBA0171"
-  //     ); // please contact to @0marSalah to add your account to the project
-  //     const folderName = "USER_UN(IQUE_KEY"; // need to discussed with team how we sill set the folder name
-  //     const folderId = await storage.createFolder(folderName);
+  const isLargeScreen = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up("sm")
+  );
 
-  //     const fileData = {
-  //       fileName: "test.json",
-  //       mimeType: "application/json",
-  //       body: new Blob([JSON.stringify({ name: "amr nabel" })], {
-  //         type: "application/json",
-  //       }),
-  //     };
-  //     const fileId = await storage.save(fileData, folderId);
-  //     console.log("File uploaded successfully with ID:", fileId);
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // }
-  // useEffect(() => {
-  //   createFolderAndUploadFile();
-  // }, []);
   return (
     <Box
       sx={{
