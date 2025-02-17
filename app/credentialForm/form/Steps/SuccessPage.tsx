060b13dc146c23d5fd7a1038118bea09e95ff219
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2025-02-17 21:04:01 +0200

    api update

--- app/credentialForm/form/Steps/SuccessPage.tsx
+++ app/credentialForm/form/Steps/SuccessPage.tsx
@@ -112,6 +112,10 @@ const SuccessPage: React.FC<SuccessPageProps> = ({
   ) => {
     const credentialLink = `https://linkedcreds.allskillscount.org/view/${fileId}`
     const credentialData = res
+    console.log('🚀 ~ handleShareOption ~ credentialData:', {
+      ...credentialData,
+      id: fileId
+    })
 
     if (option === 'LinkedTrust') {
       fetch('https://dev.linkedtrust.us/api/credential', {
