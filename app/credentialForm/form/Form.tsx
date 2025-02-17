84c76868dba3a8514c8421fa01499a8f7e5a9afb
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2025-02-17 15:15:17 +0200

    share with linked trust and solve the err with the sskill page

--- app/credentialForm/form/Form.tsx
+++ app/credentialForm/form/Form.tsx
@@ -35,6 +35,7 @@ const Form = ({ onStepChange }: any) => {
   const [fileId, setFileId] = useState('')
   const [image, setImage] = useState('')
   const [selectedFiles, setSelectedFiles] = useState<any[]>([])
+  const [res, setRes] = useState<any>(null)
 
   const characterLimit = 294
   const { data: session } = useSession()
@@ -192,6 +193,7 @@ const Form = ({ onStepChange }: any) => {
       setFileId(`${file.id}`)
 
       console.log('🚀 ~ handleFormSubmit ~ res:', res)
+      setRes(res)
       return res
     } catch (error: any) {
       console.error('Error during signing process:', error)
@@ -314,6 +316,7 @@ const Form = ({ onStepChange }: any) => {
                     fileId={fileId}
                     storageOption={watch('storageOption')}
                     selectedImage={image}
+                    res={res}
                   />
                 </Box>
               </Slide>
